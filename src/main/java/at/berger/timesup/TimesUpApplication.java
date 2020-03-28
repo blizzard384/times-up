32ab432b16f276731311bd52d82026f816ce7e21
Jan Berger <john-berger@seznam.cz>
2020-03-28 18:29:54 +0100

    make it better

---
+++ src/main/java/at/berger/timesup/TimesUpApplication.java
@@ -0,0 +1,13 @@
+package at.berger.timesup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimesUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/berger/timesup/TimesUpApplicationTests.java
@@ -0,0 +1,13 @@
+package at.berger.timesup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
